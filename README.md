8c10ff99b501e45b7fe76c1be31acf98ed9baec4
Ben <ben_jacobson@live.com>
2021-07-12 20:52:16 +1000

    Got SDL and SDL_TTF working within a window

--- README.md
+++ README.md
@@ -5,15 +5,18 @@ This will eventually be worked into a nes emulator, in due time. For the mean ti
 
 - C++ 17, to be compiled with G++
 - SDL Version 2.0.14 
-- For Windows, the 32 bit version of SDL.dll. Place this into the same directory as nes_emulator.exe. Alternatively, you can place this into 32 bit DLL into your /System32 folder and the 64 bit DLL into the Syswow64 folder however I have noticed issues when doing this. If the application first encounters the 64 bit version in it's path, it can crash on startup. Therefore try to avoid doing this for the time being.  
+- SDL_TTF Version 2.0.15
+- For Windows, the 32 bit version of SDL.dll and all dll files found in SDLTTf bin too (SDL2_ttf.dll, zlib1.dll, libfreetype-6.dll, etc). Place this into the same directory as nes_emulator.exe. Alternatively, you can place this into 32 bit DLL into your /System32 folder and the 64 bit DLL into the Syswow64 folder however I have noticed issues when doing this. If the application first encounters the 64 bit version in it's path, it can crash on startup. Therefore try to avoid doing this for the time being.  
 
 # Compile instructions
 
-For Linux, install SDL2 as per the vendor instructions for your Linux distro.
+For Linux, install SDL2 and SDL2_TTF as per the vendor instructions for your Linux distro.
 For Windows 10, our makefile relies on using a symbolic links to link the project to the SDL library. Once you have downloaded SDL library for Mingw, create these with the following commands: 
 
 >  ln -s /Path/To/SDL2/Lib/Folder lib/SDL2
 >  ln -s /Path/To/SDL2/Include/Folder include/SDL2
+>  ln -s /Path/To/SDL2TTF/Lib/Folder lib/SDL2TTF
+>  ln -s /Path/To/SDL2TTF/Include/Folder include/SDLTTF
 
 Once that's set up, simply use the makefile to compile to your chosen platform. 
 > make
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #define SDL_MAIN_HANDLED
 #include <SDL2/SDL.h>
+#include <SDL2TTF/SDL_ttf.h>
 
 #include <iostream>
 #include <iomanip>
+#include <string>
 
 #include "bus.h"
 #include "cpu.h"
@@ -17,26 +19,73 @@ SDL_Window *test_window = NULL;
 
 int main()
 {
+	// initialize our bus, ram and cpu
+	bus nes_bus;
+	ram nes_ram(&nes_bus, RAM_SIZE_BYTES, 0x000, 0x7FF); // temporarily mapped to 0x000 through to 0x7FF for now
+	cpu nes_cpu(&nes_bus, &nes_ram);  // todo, add the PPU
+
 	// Attempt to init SDL
     if (SDL_Init(SDL_INIT_VIDEO) < 0) {        
 		std::cout << "SDL could not initialize! SDL_Error: " << SDL_GetError() << std::endl;
 		return 0;
     }
 
-    // On success, setup SDL
+	// start by creating a base path
+	char *base_path = SDL_GetBasePath();
+	//size_t base_path_len = strlen(base_path);
+	
+    if (!base_path) {
+        base_path = SDL_strdup("./");
+    } 
+
+	// std::cout << "Base path set at: " << test << std::endl;
+
+    // On success, setup SDL window and rendered
 	SDL_Window* window = SDL_CreateWindow("6502 Emulator", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
-	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // black
+	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255); // black, full alpha
 	SDL_RenderClear(renderer);
 	SDL_RenderPresent(renderer);
 
-	// https://stackoverflow.com/questions/22886500/how-to-render-text-in-sdl2
+	// initialize our TTF font
+	TTF_Init();
+	std::string font_file_name = ((std::string)base_path).append("FreeSans.ttf");		// a bit of a casting nightmare to say the least, but it works. 
+	TTF_Font* Sans_font = TTF_OpenFont(font_file_name.c_str(), 24);
 
-	bus nes_bus;
-	ram nes_ram(&nes_bus, RAM_SIZE_BYTES, 0x000, 0x7FF); // temporarily mapped to 0x000 through to 0x7FF for now
-	cpu nes_cpu(&nes_bus, &nes_ram);  // todo, add the PPU
+    if (Sans_font == NULL) {
+		std::cout << "error - font not found, error code: " << TTF_GetError() << std::endl;    	 
+	    exit(EXIT_FAILURE);
+    }
 
-    std::system("clear");
+	SDL_Color White = {255, 255, 255, 255}; // full white, full alpha
+	SDL_Surface* surfaceMessage = TTF_RenderText_Solid(Sans_font, "Hello SDL!", White); 
+	
+	// Convert the message into a texture
+	SDL_Texture* Message = SDL_CreateTextureFromSurface(renderer, surfaceMessage);
+
+	SDL_Rect Message_rect; //create a rect
+	Message_rect.x = 10;  //controls the rect's x coordinate 
+	Message_rect.y = 10; // controls the rect's y coordinte
+	Message_rect.w = 110; // controls the width of the rect
+	Message_rect.h = 110; // controls the height of the rect
+
+	bool quit = false; //Main loop flag
+	SDL_Event event_handler; //Event handler
+
+	//While application is running
+	while (!quit) {
+		while (SDL_PollEvent(&event_handler) != 0) {//Handle events on queue
+			if (event_handler.type == SDL_QUIT) {			//User requests quit
+				quit = true;
+			}
+		}
+
+		SDL_RenderClear( renderer );
+		SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
+		SDL_RenderPresent(renderer);
+	}
+
+    /* std::system("clear");
 
 	// Output ram content, with the first column being address offset. 
 	std::cout << "RAM contents:" << std::endl; // 6 spaces to offset the header row.
@@ -49,9 +98,18 @@ int main()
 			std::cout << "0x" << std::setfill('0') << std::setw(2) << i << ": "; 			
 		}		
 		std::cout << std::setfill('0') << std::setw(2) << (uint16_t)(nes_ram.debug_read(i)) << " ";  // seems that we need to cast to uint16_t to allow the fill and width to work properly. 
-	}
+	}*/ 
 
-//	while (true) {
-		// nes_cpu.cycle(); // the cpu will manage it's own clock pulses, and will fetch / decode data as needed. It will also prompt the other devices on the bus to do their thing, e.g just like a real CPU can trigger SS/CE on devices via address bus decoding
-//	}
+	// tidy up
+	TTF_CloseFont(Sans_font);
+
+	SDL_FreeSurface(surfaceMessage);
+	SDL_DestroyTexture(Message);
+	TTF_Quit();
+
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
