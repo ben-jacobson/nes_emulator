aa22a340839c17cdd5e3a370dff59ada1e6fa774
ben-jacobson <ben_jacobson@live.com>
2021-07-21 20:42:48 +1000

    created instruction decoder code, the instruction trace now shows actual code in ROM and points to current instruction

--- include/instr_trace_graphics.h
+++ include/instr_trace_graphics.h
@@ -1,22 +1,30 @@
 #pragma once
 
 #include <SDL2/SDL_ttf.h>
-#include "status_graphics.h"
-#include "bus.h"
 #include <iomanip>
 
+#include "status_graphics.h"
+#include "bus.h"
+#include "cpu.h"
 
 class instr_trace_graphics : public status_graphics
 {
 public:
-	instr_trace_graphics(bus* bus_ptr, uint16_t start_address, SDL_Renderer* renderer, const char* font_filename, int ptsize, uint16_t preset_display_x, uint16_t preset_display_y);
+	instr_trace_graphics(cpu* cpu_ptr, bus* bus_ptr, uint16_t start_address, SDL_Renderer* renderer, const char* font_filename, int ptsize, uint16_t preset_display_x, uint16_t preset_display_y);
 	~instr_trace_graphics() = default;
 
     void display_contents();
 
 private:
     static constexpr uint8_t INSTRUCTIONS_TO_DISPLAY = 12;
-    uint16_t _start_address;
-    bus* _bus_ptr;
+    uint16_t _start_address, _current_address;
+    uint8_t last_instruction_width;
+    std::string _decoded_instruction;
+
     uint16_t _preset_display_x, _preset_display_y; 
+
+    bus* _bus_ptr;
+    cpu* _cpu_ptr; // for viewing the instruction opcodes + address modes data
+
+    void fetch_and_decode_next_instruction(void);
 };
--- src/instr_trace_graphics.cpp
+++ src/instr_trace_graphics.cpp
@@ -1,9 +1,10 @@
 #include "instr_trace_graphics.h"
 
-instr_trace_graphics::instr_trace_graphics(bus* bus_ptr, uint16_t start_address, SDL_Renderer* renderer, const char* font_filename, int ptsize, uint16_t preset_display_x, uint16_t preset_display_y)
+instr_trace_graphics::instr_trace_graphics(cpu* cpu_ptr, bus* bus_ptr, uint16_t start_address, SDL_Renderer* renderer, const char* font_filename, int ptsize, uint16_t preset_display_x, uint16_t preset_display_y)
     : status_graphics(renderer, font_filename, ptsize)
 {
     _bus_ptr = bus_ptr;
+    _cpu_ptr = cpu_ptr;
     
     _preset_display_x = preset_display_x;
     _preset_display_y = preset_display_y;
@@ -18,8 +19,42 @@ void instr_trace_graphics::display_contents() {
     set_colour({255, 255, 255, 255}); // green
     draw_to_buffer("Instruction Trace", _preset_display_x + 20, _preset_display_y);
 
+    std::string decoded_instruction;
+    _current_address = _start_address - (INSTRUCTIONS_TO_DISPLAY / 2) + 1; // reset back to the start of where to view the trace.
+
     for (uint8_t i = 0; i < INSTRUCTIONS_TO_DISPLAY; i++) {
-        _bus_ptr->set_address(_start_address + i);
-        draw_to_buffer("0x8000: 0xBB 0xAA 0xFF. INDX: STX 126 50", _preset_display_x + 20, _preset_display_y + ((i + 1) * _font_height));
+        _bus_ptr->set_address(_current_address);
+        fetch_and_decode_next_instruction();
+        draw_to_buffer(_decoded_instruction, _preset_display_x + 20, _preset_display_y + ((i + 1) * _font_height));
     }
 }
+
+void instr_trace_graphics::fetch_and_decode_next_instruction(void) {
+    uint16_t current_address = _bus_ptr->read_address();
+    uint8_t opcode = _bus_ptr->read_data(); // we are already at the address
+    std::string instruction_name = _cpu_ptr->_opcode_decoder_lookup[opcode].name;
+    std::string address_mode = _cpu_ptr->_opcode_decoder_lookup[opcode].address_mode_name;
+    uint8_t instruction_bytes = _cpu_ptr->_opcode_decoder_lookup[opcode].instruction_bytes;
+    uint8_t operand; 
+    
+
+    // "0x8000: 0xBB 0xAA 0xFF. INDX: STX 126 50"
+
+    std::stringstream decoded_line;
+    decoded_line << "0x" << std::setfill('0') << std::setw(4) << std::uppercase << std::hex << current_address << ": ";
+    decoded_line << "0x" << std::setfill('0') << std::setw(2) << std::uppercase << std::hex << (uint16_t)opcode << " " << instruction_name << " ";
+
+    if (instruction_bytes > 1) {
+        for (uint8_t i = 0; i < instruction_bytes - 1; i++) {   // print as many operands as needed
+            _current_address++;
+            _bus_ptr->set_address(_current_address);
+            operand = _bus_ptr->read_data();
+            decoded_line << "0x" << std::setfill('0') << std::setw(2) << std::uppercase << std::hex << operand << " ";
+        }
+    }
+
+    decoded_line << "[" << address_mode << "]"; 
+    _decoded_instruction = decoded_line.str();
+    _current_address++;
+}
+
